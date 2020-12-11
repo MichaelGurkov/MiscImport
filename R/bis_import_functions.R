19ba06246cf34ef0bc774548cb55f76664306967
MichaelGurkov <michael.gurkov@mail.huji.ac.il>
2020-12-11 22:30:52 +0200

    initial commit - import_bis_policy_rates function

---
+++ R/bis_import_functions.R
@@ -0,0 +1,42 @@
+#' This function imports policy rates from BIS format
+#'
+#' @importFrom  readr read_csv
+#'
+#' @importFrom zoo as.yearmon
+#'
+#' @importFrom stats complete.cases
+#'
+#' @importFrom rlang .data
+#'
+#' @importFrom  tidyr pivot_longer
+#'
+#' @import magrittr
+#'
+#' @import stringr
+#'
+#' @import dplyr
+#'
+#' @param file_path string a file path to the source data file
+#'
+#' @export
+#'
+import_bis_policy_rates = function(file_path){
+
+  . = NULL
+
+  raw_df = read_csv(file_path)
+
+  clean_df = raw_df %>%
+    select(-.data$FREQ,
+           -.data$REF_AREA,
+           -.data$`Time Period`,
+           -.data$Frequency) %>%
+    rename(country = .data$`Reference area`) %>%
+    pivot_longer(-.data$country, names_to = "date",
+                 values_to = "policy_rate") %>%
+    filter(complete.cases(.)) %>%
+    mutate(date = as.yearmon(.data$date)) %>%
+    mutate(country = str_replace_all(.data$country, "\\s", "_"))
+
+
+}
