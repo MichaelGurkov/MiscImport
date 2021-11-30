e9cbfbef1db77cfb9a56d0dcac4c06d41667995c
Michael Gurkov <michael.gurkov@mail.huji.ac.il>
2021-11-30 22:42:00 +0200

    add BOI import functions

--- R/boi_import_functions.R
+++ R/boi_import_functions.R
@@ -212,6 +212,58 @@ import_boi_credit_df = function(credit_df_filepath = NULL,
 
 }
 
+#' This function imports credit data from BOI format file
+#'
+#' @import readxl
+#'
+#' @import tidyr
+#'
+#' @import dplyr
+#'
+#' @import lubridate
+#'
+#' @export
+#'
+import_boi_debt_df = function(debt_df_filepath = NULL,
+                                data_frequency = "month"){
+
+  if(is.null(debt_df_filepath)){
+
+    debt_df_filepath = paste0(
+      file.path(Sys.getenv("USERPROFILE")),
+      "\\OneDrive - Bank Of Israel\\Data\\",
+      "BoI\\Credit\\itrchovh.xlsx")
+  }
+
+  sheets_names = excel_sheets(debt_df_filepath)
+
+
+  data_format = return_datafile_format(data_freq = "month",
+                                       data_type = "debt")
+
+
+  if(data_frequency == "month"){
+
+    monthly_df = read_xlsx(debt_df_filepath,sheet = sheets_names[2],
+                           range = cell_limits(ul = c(8,1),
+                                               lr = c(77,NA_integer_)))
+
+    df = monthly_df %>%
+      slice(data_format$row_num) %>%
+      select(-1) %>%
+      bind_cols(select(data_format, -row_num)) %>%
+      pivot_longer(-c("lender","borrower","instrument","category"),
+                   names_to = "date", values_to = "value") %>%
+      mutate(date = zoo::as.yearmon(date, format = "%m-%y"))
+
+  }
+
+  return(df)
+
+
+
+}
+
 
 #' This function imports debt by sectors data from BOI format file
 #'
@@ -382,3 +434,64 @@ import_boi_corp_bond_market_data = function(file_path = NULL){
 
 }
 
+
+
+# This function return data format
+
+return_datafile_format = function(data_freq = "month",
+                                  data_type = "debt"){
+
+  month_debt_format = tibble::tribble(
+    ~row_num,~lender,~borrower,~instrument,~category,
+    "2","banks","business_sector","all_instruments","na",
+    "3","banks","business_sector","loans","na",
+    "4","banks","business_sector","traded_bonds","na",
+    "5","institutional","business_sector","all_instruments","na",
+    "6","institutional","business_sector","loans","na",
+    "7","institutional","business_sector","traded_bonds","na",
+    "8","institutional","business_sector","non_traded_bonds","na",
+    "9","credit_card","business_sector","all_instruments","na",
+    "10","credit_card","business_sector","loans","na",
+    "12","foreign","business_sector","all_instruments","na",
+    "13","foreign","business_sector","loans","na",
+    "14","foreign","business_sector","traded_bonds","na",
+    "15","foreign","business_sector","non_traded_bonds","na",
+    "17","gov_sector","business_sector","all_instruments","na",
+    "18","gov_sector","business_sector","allocated_credit","na",
+    "19","households","business_sector","all_instruments","na",
+    "20","households","business_sector","traded_bonds","na",
+    "29","banks","households_sector","na","total",
+    "30","banks","households_sector","na","non_residental",
+    "32","banks","households_sector","na","residental",
+    "33","institutional","households_sector","na","total",
+    "34","institutional","households_sector","na","non_residental",
+    "35","institutional","households_sector","na","residental",
+    "36","credit_card","households_sector","na","total",
+    "37","credit_card","households_sector","na","non_residental",
+    "39","foreign","households_sector","na","total",
+    "40","foreign","households_sector","na","non_residental",
+    "55","banks","gov_sector","all_instruments","na",
+    "56","banks","gov_sector","loans","na",
+    "57","banks","gov_sector","traded_bonds","na",
+    "58","institutional","gov_sector","all_instruments","na",
+    "59","institutional","gov_sector","loans","na",
+    "60","institutional","gov_sector","traded_bonds","na",
+    "61","institutional","gov_sector","non_traded_bonds","na"
+  ) %>%
+    mutate(across(everything(), ~na_if(.,"na"))) %>%
+    mutate(row_num = as.numeric(row_num))
+
+
+
+  if(data_freq == "month" & data_type == "debt"){
+
+
+    data_file_format = month_debt_format
+
+  }
+
+return(data_file_format)
+
+
+
+}
